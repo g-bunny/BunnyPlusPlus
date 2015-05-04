e1d8029975ba00a54095dfe45167e946e4871756
g-bunny <astarrynite@gmail.com>
2015-05-04 10:39:58 -0400

    new class for inheritance lines between frames

---
+++ src/ConnectionLines.cpp
@@ -0,0 +1,9 @@
+//
+//  ConnectionLines.cpp
+//  BunnyPlusPlus
+//
+//  Created by Miri Park on 5/4/15.
+//
+//
+
+#include "ConnectionLines.h"
---
+++ src/ConnectionLines.h
@@ -0,0 +1,14 @@
+//
+//  ConnectionLines.h
+//  BunnyPlusPlus
+//
+//  Created by Miri Park on 5/4/15.
+//
+//
+
+#ifndef __BunnyPlusPlus__ConnectionLines__
+#define __BunnyPlusPlus__ConnectionLines__
+
+#include <stdio.h>
+
+#endif /* defined(__BunnyPlusPlus__ConnectionLines__) */
