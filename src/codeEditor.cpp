da71e2a6ef53fc6125eee67babf0b23c1fdc49ce
g-bunny <astarrynite@gmail.com>
2015-05-05 13:34:52 -0400

    added codeEditor window

---
+++ src/codeEditor.cpp
@@ -0,0 +1,34 @@
+//
+//  codeEditor.cpp
+//  BunnyPlusPlus
+//
+//  Created by Miri Park on 5/4/15.
+//
+//
+
+#include "CodeEditor.h"
+
+CodeEditor::CodeEditor(int xPos, int yPos, int width, int height){
+    this->xPos = xPos;
+    this->yPos = yPos;
+    this->width = width;
+    this->height = height;
+    this->setup();
+}
+
+void CodeEditor::setup(){
+    
+//    xPos = 800;
+//    yPos = 50;
+//    width = 495;
+//    height = 800;
+}
+
+void CodeEditor::draw(){
+    ofSetColor(255,255,255);
+    ofRect(xPos, yPos, width, height);
+    ofSetColor(100, 100, 100);
+    ofRect(xPos + 10, yPos + 10, width - 20, 10);
+    textAreaWidth = width - 20;
+    textStart = xPos - 10;
+}
---
+++ src/codeEditor.h
@@ -0,0 +1,24 @@
+//
+//  codeEditor.h
+//  BunnyPlusPlus
+//
+//  Created by Miri Park on 5/4/15.
+//
+//
+
+#pragma once
+#include "ofMain.h"
+
+class CodeEditor{
+public:
+    CodeEditor(int xPos, int yPos, int width, int height);
+    void setup();
+    void draw();
+    
+    int xPos, yPos, width, height;
+    int textAreaWidth;
+    int textAreaHeight;
+    int textStart;
+    
+
+};
